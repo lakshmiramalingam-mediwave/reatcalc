903fab698c96ac0488d531c15ba69b89a36bc991
lakshmiramalingam-mediwave <lakshmi_ramalingam@mindwaveventures.com>
2023-10-04 12:32:07 +0530

    added reference

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator using vite and react
+
+# https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable
--- app/src/Message.tsx
+++ app/src/Message.tsx
@@ -1,49 +1,4 @@
 
-// import { useState } from "react";
-
-// function Message(){
-//     const [firstName, setFirstName] = useState('');
-//     const [secondName, setSecondName] = useState('');
-//     const [ButtonName, setButtonName] = useState('');
-
-
-    
-//     return <div className="totaldiv">
-//         <div className="input-values">
-//         <label htmlFor="first-value">ENTER THE FIRST NUMBER</label>
-//         <input 
-//         type="number" 
-//         name="first-value"
-//          id="first-value"
-//          value={firstName}
-//          onChange={e => setFirstName(e.target.value)}/>
-
-//         <label htmlFor="second-value">ENTER THE SECOND NUMBER</label>
-//         <input 
-//         type="number"
-//          name="second-value" 
-//          id="second-value"
-//          value={secondName}
-//          onChange={e => setSecondName(e.target.value)}/> 
-//         </div>
-//         <div className="button ">
-//         <button id="add">Add</button>
-//         <button id="sub">Sub</button>
-//         <button id="mul">Mul</button>
-//         <button id="divide">Divide</button>
-//         {/* value={ButtonName}
-//          onChange={e => setButtonName(e.target.value)} */}
-//         </div>
-        
-//     </div>
-// }
-// export default Message;
-
-
-
-
-
-
 import { useState } from "react";
 function Calc() {
   const [numberOne, setNumberOne] = useState(0);
@@ -92,8 +47,6 @@ function Calc() {
   );
 }
 export default Calc;
-
-
 
 
 
